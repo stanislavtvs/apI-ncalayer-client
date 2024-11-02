6d7ed4dc9a59194a70ccf92f99523d431b217364
stanislavtvs <65898335+stanislavtvs@users.noreply.github.com>
2024-11-03 01:21:58 +0500

    editing according to js standards

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
-function errortext(text) {
+function displaysErrorText(text) {
     document.getElementById("error").innerHTML = text;
 }
-function clearerrortext() {
+
+function eraseErrorText() {
     document.getElementById("error").innerHTML = "";
 }
 
-async function connectAndSign(base64EncodedData) {
+async function connectionsToNcalayer(base64EncodedData) {
     const ncalayerClient = new NCALayerClient();
   
     try {
       await ncalayerClient.connect();
     } catch (error) {
-        errortext(`Не удалось подключиться к NCALayer: ${error.toString()}`);
+        displaysErrorText(`Не удалось подключиться к NCALayer: ${error.toString()}`);
       return;
     }
   
@@ -19,7 +20,7 @@ async function connectAndSign(base64EncodedData) {
     try {
       activeTokens = await ncalayerClient.getActiveTokens();
     } catch (error) {
-      alert(error.toString());
+      displaysErrorText(error.toString());
       return;
     }
   
@@ -29,58 +30,58 @@ async function connectAndSign(base64EncodedData) {
     try {
       base64EncodedSignature = await ncalayerClient.createCAdESFromBase64(storageType, base64EncodedData);
     } catch (error) {
-      alert(error.toString());
+      displaysErrorText(error.toString());
       return;
     }
   
     return base64EncodedSignature;
 }
 
-  function someFunc(json,type) {
+function finalDataPreparationAndOutput(json,type) {
     console.log(json)
     if (type == "bs4") {
-        success_get_nonce(json.nonce)
+        takingTheKeyAndAuthorization(json.nonce)
     }
     else if (type == "user") {
-        console.log(json);
         if (json.subjectStructure[0][0].value != "") {
-            clearerrortext();
+            eraseErrorText();
         }
         document.getElementById("name_and_surname").innerHTML = "Фамилия и имя: "+json.subjectStructure[0][0].value;
         document.getElementById("iin").innerHTML = "ИИН : "+json.userId.substring(3);
     }
-  }
+}
 
-
-async function auth(data,type) {
+async function authorization(data,type) {
 
     await fetch('https://sigex.kz/api/auth', {
+        credentials: "same-origin",
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-      }).then(async response => someFunc(await response.json(),type))
+      }).then(async response => finalDataPreparationAndOutput(await response.json(),type))
 }
-function get_nonce() {
+
+function getNonce() {
     const dataUser = {};
-    auth(dataUser,"bs4");
+    authorization(dataUser,"bs4");
 }
-function get_user(base64,token) {
+
+function getUser(base64,token) {
     const dataUser = {
         "nonce": base64,
         "signature": token,
         "external": false
     };
-    auth(dataUser,"user")
+    authorization(dataUser,"user")
 }
 
-
-function success_get_nonce(result) {
-    result_connectAndSign = connectAndSign(result)
-    result_connectAndSign.then(onFulfilled => get_user(result,onFulfilled))
-    result_connectAndSign.then(null, onRejected => console.log("Ошибка :"+onRejected))
+function takingTheKeyAndAuthorization(result) {
+    resultConnectionsToNcalayer = connectionsToNcalayer(result)
+    resultConnectionsToNcalayer.then(onFulfilled => getUser(result,onFulfilled))
+    resultConnectionsToNcalayer.then(null, onRejected => displaysErrorText("Ошибка :"+onRejected))
 }
 
 var btn = document.getElementById("btn");
-btn.addEventListener('click', get_nonce);
+btn.addEventListener('click', getNonce);
